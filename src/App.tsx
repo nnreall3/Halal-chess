a04afa5e757721659305514986718842042cf1a8
nnreall3 <nnreall3@gmail.com>
2026-02-09 20:28:24 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <BrowserRouter basename="/Halal-chess">
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="/create" element={<CreateGame />} />
