aeb39879a4cae5fbae64bcb5a81041675517af06
nnreall3 <nnreall3@gmail.com>
2026-02-09 19:50:32 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/',
+  base: '/Halal-chess/',
   server: {
     host: "::",
     port: 8080,
